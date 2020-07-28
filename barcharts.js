aad4a41ea09ca1dad7a9927031e0fafdfd92dc54
anambia <akshay.j.nambiar@aexp.com>
2020-07-28 16:41:14 +0530

    c

---
+++ barcharts.js
@@ -0,0 +1,134 @@
+
+function barcharts() {
+    var div2 = d3.select("#div2").style("display", "block");
+    div2.selectAll("*").remove();
+    div2.append("p").attr("id", "#p2").text("Bar charts");
+
+    var d1;
+    var allGroup = ["Confirmed", "Active", "Recovered", "Deceased"]
+    var sel = div2.append('div').attr("id", "#selectButton1").append("select").attr("id", "#selectButton")
+
+    sel.selectAll('myOptions')
+            .data(allGroup)
+        .enter()
+            .append('option')
+        .text(function (d) { return d; })
+        .attr("value", function (d) { return d; }) 
+        
+
+    d3.selectAll("#svg1").selectAll("*").remove();
+
+    var myColor = d3.scale.ordinal()
+      .domain(allGroup)
+      .range(['#ff0000','#1111ee', '#11aa11', '#000']);
+ 
+
+    var div = d3.select("#div2").append("div")	
+        .attr("class", "tooltip")
+        .attr("width", "70px")			
+        .style("opacity", 0);
+
+    d3.json("data.json", function(error, d) {
+        d1 = d.cases_time_series;
+        var data = d1.map(function(d) {return {"value": d.dailyconfirmed, "key": d.date};})
+        var grp = "Confirmed";
+        updateBar(data, grp)
+    }); 
+    var linearScale = false;
+    
+    
+    function updateBar(data, grp) {
+        base = linearScale?0:1;
+        div2.selectAll('svg').remove();
+        var svg = div2.append("svg").attr("id","#svg1").attr("width", 1000).attr("height", 500),
+        margin = 200,
+        width = svg.attr("width") - margin,
+        height = svg.attr("height") - margin;
+
+        var g = svg.append("g")
+        .attr("transform", "translate(" + 100 + "," + 100 + ")");
+
+
+        svg.append("text")
+            .attr("transform", "translate(0,0)")
+            .attr("x", 50)
+            .attr("y", 50)
+            .attr("font-size", "24px")
+            .text(grp + " Cases from 30th January to 26th July")        
+        
+        console.log(d3.max(data, function(d) {return parseInt(d.value); }));
+        var xScale = d3.scale.ordinal().domain(data.map(function(d) { return d.key; })).rangeRoundBands([0, width], 0.1);
+        var yScale = d3.scale.log().domain([1, d3.max(data, function(d) { return parseInt(d.value); })]).range([height, 0]).nice();
+        if (linearScale) {
+            yScale = d3.scale.linear().domain([d3.min(data, function(d) { return parseInt(d.value); }), d3.max(data, function(d) { return parseInt(d.value); })]).range([height, 0]).nice();
+        }
+        var xAxis = d3.svg.axis()
+        .scale(xScale)
+        .orient("bottom");
+       var yAxis =  d3.svg.axis()
+       .scale(yScale)
+       .orient("left");    
+        
+        g.append("g")
+         .attr("transform", "translate(10," + height + ")")
+         .append("text")
+         .attr("y", height - 250)
+         .attr("x", width - 100)
+         .attr("text-anchor", "end")
+         .attr("stroke", "black")
+         .text("Date");
+
+         g.append("g")
+         .call(yAxis.ticks(null).tickSize(0))
+         .append("text")
+         .attr("transform", "rotate(-90)")
+         .attr("y", 6)
+         .attr("dy", "-5.1em")
+         .attr("text-anchor", "end")
+         .attr("stroke", "black")   
+         .text(grp);
+
+         g.selectAll(".bar")
+         .data(data)
+         .enter().append("rect")
+         .attr("class", "bar")
+         .attr("x", function(d) { return xScale(d.key); })
+         .attr("y", function(d) { return d.value > 0 ? yScale(d.value): yScale(base); })
+         .attr("width", 3)
+         .attr("height", function(d) { if(d.value < 0 && linearScale) {
+            return (yScale(base)) - yScale(-1*d.value);;
+         }
+            return  (yScale(base)) - yScale(d.value); })
+         .attr("fill", function(d){ return myColor(grp) })
+         .on("mouseover", function(d) {		
+            div.transition()		
+                .duration(200)		
+                .style("opacity", 1);		
+            div	.html("<table> <tr><td>"+ grp + " : </td><td>" + (!d.value ? "NA" : d.value) + "</td></tr>"  +
+                      "<tr><td>Date : </td><td>" + (!d.key? "NA" : d.key) + "</td></tr></table>")	
+                .style("left", (d3.event.pageX) + "px")		
+                .style("top", (d3.event.pageY - 28) + "px");	
+            })					  
+            .on("mouseout", function(d) {		    
+                    div.transition()		  
+                        .duration(500)		
+                        .style("opacity", 0);	  
+            })
+         ;
+         
+    }
+    console.log("d3.select('#selectButton1') : " + sel);
+
+    sel.on('change', function() {
+        console.log("here1")    
+        var selectedOption = d3.select(this).property("value")
+        var data ;
+        console.log("select" + selectedOption);
+        if(selectedOption == 'Confirmed') data = d1.map(function(d) {return {"value": parseInt(d.dailyconfirmed), "key": d.date};});
+        if(selectedOption == 'Active') data = d1.map(function(d) {return {"value": parseInt(d.dailyconfirmed) - parseInt(d.dailyrecovered), "key": d.date};});
+        if(selectedOption == 'Recovered') data = d1.map(function(d) {return {"value": parseInt(d.dailyrecovered), "key": d.date};});
+        if(selectedOption == 'Deceased') data = d1.map(function(d) {return {"value": parseInt(d.dailydeceased), "key": d.date};});
+        updateBar(data, selectedOption)
+    });
+    
+}   
---
+++ cluster.js
@@ -0,0 +1,200 @@
+function showClusters() {
+    var width = 1160,
+        height = 800,
+        root;
+  
+    d3.select("#div2").style("display", "block")
+    d3.select("#div2").html(`
+    <p id="p2"></p>
+    <fieldset style="width : 50px">
+    <legend>
+    Legend
+    </legend>
+    <table>
+      <tr>
+        <td>
+      <svg height="10" width="10">
+        <circle cx=5 cy=5 r="400" stroke="#000" stroke-width="1" fill="#11aa11" />
+      </svg> </td>
+      <td><span>Recovered</span></td>
+      </tr>
+      <tr>
+        <td>
+      <svg height="10" width="10">
+        <circle cx=5 cy=5 r="400" stroke="#000" stroke-width="1" fill="#ff0000" />
+      </svg> </td>
+      <td><span>Confirmed/Hospitalised</span></td>
+      </tr>
+      <tr>
+        <td>
+      <svg height="10" width="10">
+        <circle cx=5 cy=5 r="400" stroke="#000" stroke-width="1" fill="#000" />
+      </svg> </td>
+      <td><span>Deceased</span></td>
+      </tr>
+    </table>
+</fieldset>
+<svg id="svg1"></svg>`)
+
+d3.select("#p2").text("All the clusters in India as of July 08, 2020");
+
+    var force = d3.layout.force()
+        .size([width, height])
+        .linkDistance(25)
+        .charge(-15)
+        .on("tick", tick);
+    d3.selectAll("#svg1").selectAll("*") .remove();
+    console.log("removed")
+    var svg = d3.select("#svg1")
+        .attr("width", width)
+        .attr("height", height)
+        .append('g')
+        .attr('transform', 'translate(' + 30 + ',0)');
+  
+    console.log(svg)
+    var link = svg.selectAll(".link"),
+        node = svg.selectAll(".node");
+  
+    var map = {};
+  
+    d3.json("raw_data_all.json", function(error, json) {
+      if (error) throw error;
+  
+      root = json;
+      update();
+    });
+    // root = JSON.parse(djson);
+    // update();
+  
+    var div = d3.select("#div2").append("div")	
+        .attr("class", "tooltip")				
+        .style("opacity", 0);
+  
+    function update() {
+      var nodes = flatten(root),
+          links = links1(root);
+      // Restart the force layout.
+      console.log(nodes);
+      console.log(links);
+      force
+          .nodes(nodes)
+          .links(links)
+          .start();
+  
+      // Update the links…
+      link = link.data(links, function(d) { return d.source.index; });
+  
+      // Exit any old links.
+      link.exit().remove();
+  
+      // Enter any new links.
+      link.enter().insert("line", ".node")
+          .attr("class", "link")
+          .attr("x1", function(d) { return d.source.x; })
+          .attr("y1", function(d) { return d.source.y; })
+          .attr("x2", function(d) { return d.target.x; })
+          .attr("y2", function(d) { return d.target.y; });
+  
+      // Update the nodes…
+      node = node.data(nodes, function(d) { return d.patientnumber; }).style("fill", color);
+  
+      // Exit any old nodes.
+      node.exit().remove();
+  
+      // Enter any new nodes.
+      node.enter().append("circle")
+          .attr("class", "node")
+          .attr("cx", function(d) { return d.x; })
+          .attr("cy", function(d) { return d.y; })
+          .attr("r", function(d) { return 5.5 ; })
+          .style("fill", color)
+          // .on("click", click) 
+          .on("mouseover", function(d) {		
+                div.transition()		
+                    .duration(200)		
+                    .style("opacity", 1);		
+                div	.html("<table> <tr><td>Patient No. : </td><td>" + (!d.patientnumber ? "NA" : d.patientnumber) + "</td></tr>"  +
+                          "<tr><td>Age : </td><td>" + (!d.agebracket? "NA" : d.agebracket) + "</td></tr>"  +
+                          "<tr><td>Contacted from : </td><td>" + (!d.contractedfromwhichpatientsuspected ? "NA" : d.contractedfromwhichpatientsuspected) + "</td></tr>"  +
+                          "<tr><td>Current status : </td><td>" + (!d.currentstatus? "NA" : d.currentstatus) + "</td></tr>"  +
+                          "<tr><td>State : </td><td>" + (!d.detectedstate? "NA" : d.detectedstate) + "</td></tr>"  +
+                          "<tr><td>Notes : </td><td>" + (!d.backupnotes? "NA" : d.backupnotes) + "</td></tr>"  +
+                          "<tr><td>Type of transmission : </td><td>" + (!d.typeoftransmission ? "NA" : d.typeoftransmission) + "</td></tr></table>"
+                    )	
+                    .style("left", (d3.event.pageX) + "px")		
+                    .style("top", (d3.event.pageY - 28) + "px");	
+                })					  
+          .on("mouseout", function(d) {		
+                div.transition()		  
+                    .duration(500)		
+                    .style("opacity", 0);	  
+          })
+          .call(force.drag);
+    }
+  
+    function tick() {
+      link.attr("x1", function(d) { return d.source.x; })
+          .attr("y1", function(d) { return d.source.y; })
+          .attr("x2", function(d) { return d.target.x; })
+          .attr("y2", function(d) { return d.target.y; });
+  
+      node.attr("cx", function(d) { return d.x; })
+          .attr("cy", function(d) { return d.y; });
+    }
+  
+    // Color leaf nodes orange, and packages white or blue.
+    function color(d) {
+      if(d.currentstatus == 'Recovered') {
+        return "#11aa11";
+      } 
+      if(d.currentstatus == 'Hospitalized') {
+        return "#ff0000";
+      }
+      if(d.currentstatus == "Deceased")
+      return "#000";
+    }
+  
+    // Toggle children on click.
+    function click(d) {
+    
+      // TODO tooltip
+  
+      // update();
+      
+    }
+  
+    // Returns a list of all nodes under the root.
+    function flatten(root) {
+      var nodes = [], i = 0;
+      for(i = 0; i< root.length;i++) {
+          nodes.push(root[i]);
+          map[root[i].patientnumber] = root[i];
+      }
+      return nodes;
+    }
+  
+    function links1(root) {
+      var links = [], i = 0;
+      for(i = 0; i< root.length;i++) {
+          if(root[i].contractedfromwhichpatientsuspected.length > 0) {
+            var arr1 = root[i].contractedfromwhichpatientsuspected.split(',')
+            var j = 0;
+            for( j = 0; j< arr1.length; j++) {
+              if (arr1[j].startsWith('P')) {
+              m = {};
+              m.source = root[i]
+              m.target = map[arr1[j]]
+              //  if(m.target != undefined) {
+                links.push(m);
+              //  }
+              } else {
+  
+              }
+            }
+          }
+      }
+      return links  ;
+    }
+  }
+  
+  
